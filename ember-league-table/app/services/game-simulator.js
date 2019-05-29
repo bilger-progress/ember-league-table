726e3e37b0d8abedd4beb30e773aaad195c6896e
Bilger Yahov <bayahov1@gmail.com>
2019-05-29 16:48:35 +0300

    Implement functionality to start simulating games.

--- ember-league-table/app/services/game-simulator.js
+++ ember-league-table/app/services/game-simulator.js
@@ -1,16 +1,38 @@
 import Service from '@ember/service';
 import { inject } from '@ember/service';
+import { later } from '@ember/runloop';
+import { shuffle } from 'ember-composable-helpers/helpers/shuffle';
 
 export default Service.extend({
     store: inject(),
     init() {
         this._super(...arguments);
         this.seedTeams();
+        later(this, this.simulateGame, 1000);
     },
     seedTeams() {
         const teamNames = ['Team 1', 'Team 2', 'Team 3', 'Team 4', 'Team 5'];
         teamNames.forEach((item, index) => {
             this.store.createRecord('team', { id: index, name: item });
         });
+    },
+    simulateGame() {
+        const teams = this.store.peekAll('team');
+        const shuffledTeams = shuffle(teams);
+        const homeTeam = shuffledTeams[0];
+        const awayTeam = shuffledTeams[1];
+        const homeGoals = this.randomScore(4);
+        const awayGoals = this.randomScore(3);
+        this.store.createRecord('game', {
+            homeTeam,
+            awayTeam,
+            homeGoals,
+            awayGoals,
+            playedOn: new Date()
+        });
+        later(this, this.simulateGame, 1000);
+    },
+    randomScore(maximumGoals){
+        return Math.round(Math.random() * maximumGoals);
     }
 });
