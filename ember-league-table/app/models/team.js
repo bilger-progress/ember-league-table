d4ba495c73a3ab04f7abf7446aa026167ad847d4
Bilger Yahov <bayahov1@gmail.com>
2019-05-30 21:48:35 +0300

    Add "playedGames" and "drawnGames" properties to each team.

--- ember-league-table/app/models/team.js
+++ ember-league-table/app/models/team.js
@@ -1,8 +1,12 @@
 import DS from 'ember-data';
+import { union, filterBy } from '@ember/object/computed';
+
 const { Model } = DS;
 
 export default Model.extend({
     name: DS.attr('string'),
     homeGames: DS.hasMany('game', { inverse: 'homeTeam' }),
-    awayGames: DS.hasMany('game', { inverse: 'awayTeam' })
+    awayGames: DS.hasMany('game', { inverse: 'awayTeam' }),
+    playedGames: union('homeGames', 'awayGames'),
+    drawnGames: filterBy('playedGames', 'isDraw')
 });
