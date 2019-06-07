c12a66004011a74302d81e017ce59088bf095393
bilger-progress <bilger.yahov@progress.com>
2019-06-07 18:18:16 +0300

    Implement the "SignInContainer" component.

--- ember-league-table/app/components/sign-in-container.js
+++ ember-league-table/app/components/sign-in-container.js
@@ -1,4 +1,20 @@
 import Component from '@ember/component';
+import { inject as service } from '@ember/service'
 
 export default Component.extend({
+    firebaseApp: service(),
+    actions: {
+        signIn() {
+            this.set('errorMsg', '');
+            this.firebaseApp.auth().then(auth =>
+                auth.signInWithEmailAndPassword(this.email, this.password)
+                    .catch(error => {
+                        this.set('errorMsg', error.message);
+                    })
+            );
+        }
+    },
+    email: '',
+    password: '',
+    errorMsg: ''
 });
