0e007cc7c687623210a094abcdba1f6c8721aa8c
bilger-progress <bilger.yahov@progress.com>
2019-06-07 16:32:55 +0300

    Set-up basic navigation based on the Firebase Auth State.

---
+++ ember-league-table/app/services/firebase-auth-state.js
@@ -0,0 +1,19 @@
+import Service from '@ember/service';
+import { inject as service } from '@ember/service'
+
+export default Service.extend({
+    firebaseApp: service(),
+    router: service(),
+    init() {
+        this._super(...arguments);
+        this.firebaseApp.auth().then(auth => auth.onAuthStateChanged(user => {
+            if (user) {
+                // TODO: Store - set User.
+                this.router.transitionTo('dashboard');
+            } else {
+                // TODO: Store - remove User.
+                this.router.transitionTo('sign-in');
+            }
+        }))
+    }
+});
